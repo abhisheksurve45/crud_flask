8e8ba0c1dffad9024e8c5fd4390766f2b8df0123
abhishek.sur <abhishek.sur@media.net>
2019-11-05 20:25:30 +0530

    added remotemysql

--- web/user_crud/db_config.py
+++ web/user_crud/db_config.py
@@ -3,8 +3,8 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'crud_db'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_USER'] = 'A6HZkAtXV1'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'axrHwbkA5n'
+app.config['MYSQL_DATABASE_DB'] = 'A6HZkAtXV1'
+app.config['MYSQL_DATABASE_HOST'] = '37.59.55.185'
 mysql.init_app(app)
